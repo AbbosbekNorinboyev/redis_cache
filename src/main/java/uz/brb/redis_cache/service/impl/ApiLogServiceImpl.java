e881118e2c0c8b48cc411bae09132d5262ea6574
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2025-11-03 11:10:10 +0500

    redis cache fix

--- src/main/java/uz/brb/redis_cache/service/impl/ApiLogServiceImpl.java
+++ src/main/java/uz/brb/redis_cache/service/impl/ApiLogServiceImpl.java
@@ -1,6 +1,7 @@
 package uz.brb.redis_cache.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -8,12 +9,9 @@ import uz.brb.redis_cache.dto.response.Response;
 import uz.brb.redis_cache.entity.ApiLog;
 import uz.brb.redis_cache.repository.ApiLogRepository;
 import uz.brb.redis_cache.service.ApiLogService;
-import uz.brb.redis_cache.service.logic.RedisCacheService;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static uz.brb.redis_cache.util.Util.localDateTimeFormatter;
 
@@ -21,7 +19,6 @@ import static uz.brb.redis_cache.util.Util.localDateTimeFormatter;
 @RequiredArgsConstructor
 public class ApiLogServiceImpl implements ApiLogService {
     private final ApiLogRepository apiLogRepository;
-    private final RedisCacheService redisCacheService;
 
     @Override
     public Response<?> saveLog(String username, String method, String path, LocalDateTime time, long duration) {
@@ -43,18 +40,15 @@ public class ApiLogServiceImpl implements ApiLogService {
     }
 
     @Override
+    @Cacheable(value = "posts", key = "#root.methodName")
     public Response<?> getAll(Pageable pageable) {
         List<ApiLog> apiLogs = apiLogRepository.findAll(pageable).getContent();
-        for (ApiLog apiLog : apiLogs) {
-            redisCacheService.saveData(String.valueOf(apiLog.getId()), apiLog, 10, TimeUnit.MINUTES);
-        }
-        List<Object> redisCacheAllData = redisCacheService.getAllData();
         return Response.builder()
                 .code(HttpStatus.OK.value())
                 .status(HttpStatus.OK)
                 .success(true)
                 .message("ApiLog list successfully found")
-                .data(redisCacheAllData)
+                .data(apiLogs)
                 .timestamp(localDateTimeFormatter(LocalDateTime.now()))
                 .build();
     }
